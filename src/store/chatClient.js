efcaa605943fc672447d29b1a30045cce8c3b547
Ryan Harmuth <ryanharmuth@gmail.com>
2016-09-13 12:49:49 +0800

    Fixed indentation

--- src/store/chatClient.js
+++ src/store/chatClient.js
@@ -63,57 +63,57 @@ function chatMiddleware(store) {
 
 function initChat(store) {
 	var socketUrl = 'http://localhost:5000';
-			let session = store.getState().session;
-      //var socketUrl = 'https://safe-atoll-11440.herokuapp.com';
+	let session = store.getState().session;
+  //var socketUrl = 'https://safe-atoll-11440.herokuapp.com';
 
-      var query = "userId=" + session.userId + "&userName=" + session.username;
+  var query = "userId=" + session.userId + "&userName=" + session.username;
 
-      if (session.lastMessageTimestamp) {
-        query = query + '&lastMessageTimeStamp=' + session.lastMessageTimestamp;
-      }
+  if (session.lastMessageTimestamp) {
+    query = query + '&lastMessageTimeStamp=' + session.lastMessageTimestamp;
+  }
 
-      const options = {transports: ['websocket'], query: query };
-      socket = io(socketUrl, options);
+  const options = {transports: ['websocket'], query: query };
+  socket = io(socketUrl, options);
 
-      pingFuncId = setInterval(function() {
-        //console.log('pinging server');
-        socket.emit(SocketEvents.Hello, {message: 'hello server'});
-      }, 30000);
+  pingFuncId = setInterval(function() {
+    //console.log('pinging server');
+    socket.emit(SocketEvents.Hello, {message: 'hello server'});
+  }, 30000);
 
-      socket.on(SocketEvents.Hello, function(data) {
-        //console.log('got hello');
-      });
+  socket.on(SocketEvents.Hello, function(data) {
+    //console.log('got hello');
+  });
 
-      socket.on(SocketEvents.Message, (data) => {
-        store.dispatch(storeAndAddMessages([data]));
-      	store.dispatch(updatelastReadTimestamp(data.timestamp));
-      	store.dispatch(persistCurrentSession());
-       	var currentChannel = store.getState().ui.chatUI.selectedChannel;
+  socket.on(SocketEvents.Message, (data) => {
+    store.dispatch(storeAndAddMessages([data]));
+  	store.dispatch(updatelastReadTimestamp(data.timestamp));
+  	store.dispatch(persistCurrentSession());
+   	var currentChannel = store.getState().ui.chatUI.selectedChannel;
 
-        if (data.type == 'Group' && currentChannel.type != 'Group') {
-        	store.dispatch(addChatReadStatus('0', true))
-        } else if (data.type == 'DirectMessage' && currentChannel.id != data.senderId) {
-          store.dispatch(addChatReadStatus(data.senderId, true))
-        }
-        if (data.type == 'DirectMessage') {
-          socket.emit(SocketEvents.MessageDeliveredConfirmation, data);
-        }
-      });
-      socket.on(SocketEvents.MessageSentConfirmation, (data) => {
-        store.dispatch(confirmMessageSent(data))
-      });
-      socket.on(SocketEvents.MessageDeliveredConfirmation, (data) => {
-        store.dispatch(confirmMessageDelivered(data));
-      });
-      socket.on(SocketEvents.OnlineStatus, (data) => {
-        store.dispatch(updateOnlineUsers(data));
-      });
-      socket.on(SocketEvents.TypingStatus, (data) => {
-        store.dispatch(updateTypingUsers(data));
-      });
-      // socket.on('connect', function() {
-      //   //console.log('connected socket. transport: ' + socket.io.engine.transport.name);
-      // });
+    if (data.type == 'Group' && currentChannel.type != 'Group') {
+    	store.dispatch(addChatReadStatus('0', true))
+    } else if (data.type == 'DirectMessage' && currentChannel.id != data.senderId) {
+      store.dispatch(addChatReadStatus(data.senderId, true))
+    }
+    if (data.type == 'DirectMessage') {
+      socket.emit(SocketEvents.MessageDeliveredConfirmation, data);
+    }
+  });
+  socket.on(SocketEvents.MessageSentConfirmation, (data) => {
+    store.dispatch(confirmMessageSent(data))
+  });
+  socket.on(SocketEvents.MessageDeliveredConfirmation, (data) => {
+    store.dispatch(confirmMessageDelivered(data));
+  });
+  socket.on(SocketEvents.OnlineStatus, (data) => {
+    store.dispatch(updateOnlineUsers(data));
+  });
+  socket.on(SocketEvents.TypingStatus, (data) => {
+    store.dispatch(updateTypingUsers(data));
+  });
+  // socket.on('connect', function() {
+  //   //console.log('connected socket. transport: ' + socket.io.engine.transport.name);
+  // });
 }
 
 module.exports = { chatMiddleware }
